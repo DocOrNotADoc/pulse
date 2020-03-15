87a23ef2e6e8bc97295d05e6049a02f90b23b960
DocOrNotADoc <leesunsinn@protonmail.com>
2020-03-16 01:08:24 +0300

    adapted carousel-slick,fixed tab-adaptation4promo

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
-    // $('.carousel__inner').slick({
-    //     speed: 1200,
-    //     prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
-    //     nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png"></button>',
-    //     responsive: [
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 dots: true,
-    //                 arrows: false,
-    //             }
-    //         }
-    //     ]
-    // });
+    $('.carousel__inner').slick({
+        speed: 300,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
@@ -34,19 +34,23 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__back');
 });
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    controls: false,
-});
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     controls: false,
+// });
 
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-});
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+// });
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+// });
+
+
+
+
 
 // Если подключить для планшетов и мобилок dots, как сделать, чтобы они добавлялись при добавлении картинок?
 // И как сделать, чтобы они отмечались соответственно перелистываемому слайду?
