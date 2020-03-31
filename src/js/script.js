514149280976ee70ad5feb93d04b873ac0325ed2
DocOrNotADoc <leesunsinn@protonmail.com>
2020-03-31 18:33:22 +0300

    phone mask

--- src/js/script.js
+++ src/js/script.js
@@ -49,6 +49,38 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn();
         });
     });
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {                
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символa!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введён адрес почты"
+                }
+            }
+        });
+    };
+    validateForms('#consult-form');
+    validateForms('#consult form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
 });
 
 // const slider = tns({
