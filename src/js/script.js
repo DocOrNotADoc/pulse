0da4f779aaca444fb50935068992bd6bb1fe2d3f
DocOrNotADoc <leesunsinn@protonmail.com>
2020-04-01 13:07:07 +0300

    php added, mail sending maded

--- src/js/script.js
+++ src/js/script.js
@@ -81,6 +81,21 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99")
+
+    $('form').submit(function() {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consult, #order').fadeout();
+            $('.overlay, #thanks').fadeIn();
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
 
 // const slider = tns({
